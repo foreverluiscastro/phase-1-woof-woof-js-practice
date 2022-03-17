8e152898503c981d13e12dcbe0612b70a33f74ea
luis <foreverluiscastro@gmail.com>
2022-03-17 14:37:19 -0400

    rendered dog card. Added comments for student support.

--- src/index.js
+++ src/index.js
@@ -9,10 +9,37 @@ function fetchDogs() {
 }
 
 function renderDogBar(dogs) {
-    dogs.forEach((dog) => {
-        const dogBar = document.getElementById("dog-bar")
-        const span = document.createElement("span")
-        span.innerText = dog.name
-        dogBar.appendChild(span)
-    })
+    dogs.map(dog => renderSpan(dog))
+}
+
+function renderSpan(dog) {
+    const dogBar = document.getElementById("dog-bar")
+    const span = document.createElement("span")
+    span.innerText = dog.name
+    span.addEventListener("click", () => {renderDog(dog)})
+    dogBar.appendChild(span)
+}
+
+function renderDog(dog) {
+    // Grabs the card
+    const dogCard = document.getElementById("dog-info")
+    // Clear the card (if there is a dog displayed)
+    dogCard.innerHTML = ""
+    // Create your elements
+    const img = document.createElement("img")
+    const h2 = document.createElement("h2")
+    const btn = document.createElement("button")
+    // Add info about dog to elements
+    img.src = dog.image
+    h2.innerText = dog.name
+    // Conditional to handle btn text
+    if (dog.isGoodDog === true) {
+        btn.innerText = "Good Dog!"
+    } else {
+        btn.innerText = "Bad Dog!"
+    }
+    // Append elements to DOM
+    dogCard.appendChild(img)
+    dogCard.appendChild(h2)
+    dogCard.appendChild(btn)
 }
